1adef9121bc93fa374848a313ca30de886ff9d54
Tanessia Gordon <nessiagordon@yahoo.com>
2015-09-15 13:32:25 -0400

    Update 2015-09-15T17:32:24.568Z

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ console.log("sfdsf");
 
 $(document).ready(function () {
 
+
+
   $('form').on('submit', function(event) {
     event.preventDefault();
     var new_entry = $('#input').val();
-    $('#list').append('<li>' + new_entry + '</li>');
+    $('#list').append('<li><input type="checkbox" class="checkbox" />' + new_entry + '</li>');
   });
+
 });
