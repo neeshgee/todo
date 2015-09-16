1a166d2a530f6946242d85622fd7ce041de09fa6
Tanessia Gordon <nessiagordon@yahoo.com>
2015-09-16 13:59:39 -0400

    final

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,10 +1,18 @@
-console.log("sfdsf");
-
 $(document).ready(function () {
 
   $('form').on('submit', function(event) {
     event.preventDefault();
     var new_entry = $('#input').val();
-    $('#list').append('<li>' + new_entry + '</li>');
+    $('#list').append('<li><input type="checkbox" class="checkbox" />' + new_entry + '</li>');
+    $('#input').val('');
   });
+
+  $('#list').on('click', 'input.checkbox' ,function (event) {
+    var li = $(this).closest('li');
+
+    if (this.checked) {
+      $('#completelist').append('<li>' + li.text() + '</li>');
+      li.hide();
+    }
+  })
 });
